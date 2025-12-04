5e5a00e73219b6f85c05a49d9f9d6a9d894f2ec8
panos ppchr <ppapachar@gmail.com>
2025-12-04 14:28:42 +0100

    update __init__.py to include additional tool library components and expand __all__ exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,47 @@
 from .merge_stock_hops import StockHopsMerger
+from .tool_library import (
+    MachiningTool,
+    ToolLibrary,
+    ToolHolderType,
+    HopsMacro,
+    BirdsmouthW41,
+    SaegeD350,
+    CastorD61,
+)
+from .hop_core import HopOperation, HopFile
+from .work_planes import WorkPlane, FreePlane
+from .machining_commands import (
+    MachiningCommand,
+    StartPoint,
+    G01,
+    EndPoint,
+    MillingCommand,
+    SawingCommand,
+    DrillingCommand,
+)
 
-__all__ = ["StockHopsMerger"]
+__all__ = [
+    # Tool Library
+    "MachiningTool",
+    "ToolLibrary",
+    "ToolHolderType",
+    "HopsMacro",
+    "BirdsmouthW41",
+    "SaegeD350",
+    "CastorD61",
+    # Core
+    "StockHopsMerger",
+    "HopOperation",
+    "HopFile",
+    # Work Planes
+    "WorkPlane",
+    "FreePlane",
+    # Machining Commands
+    "MachiningCommand",
+    "StartPoint",
+    "G01",
+    "EndPoint",
+    "MillingCommand",
+    "SawingCommand",
+    "DrillingCommand",
+]
